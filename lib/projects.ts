834a24a106df15fd75193144e418fceaba83f1d0
elyoit <elliotnlai@gmail.com>
2026-06-10 16:09:36 -0700

    Update projects.ts

--- lib/projects.ts
+++ lib/projects.ts
@@ -56,19 +56,23 @@ export const projectsData = [
   {
     id: 'project4',
     title: 'Project 4',
-    heroImage: '/architecture/project4/image1.png',
-    paragraphs: [""],
+    heroImage: '/architecture/project4/hero.jpg',
+    paragraphs: ["This project is a design school near Deep Eddy Pool in Austin."],
     gallery: [
-      { src: '/architecture/project4/image1.png', caption: "Default Caption" },
-      { src: '/architecture/project4/image2.png', caption: "Default Caption" },
-      { src: '/architecture/project4/image3.png', caption: "Default Caption" },
-      { src: '/architecture/project4/image4.png', caption: "Default Caption" },
-      { src: '/architecture/project4/image5.png', caption: "Default Caption" },
-      { src: '/architecture/project4/image6.png', caption: "Default Caption" },
-      { src: '/architecture/project4/image7.png', caption: "Default Caption" },
-      { src: '/architecture/project4/image8.png', caption: "Default Caption" },
-      { src: '/architecture/project4/image9.png', caption: "Default Caption" },
-      { src: '/architecture/project4/image10.png', caption: "Default Caption" }
+      { src: '/architecture/project4/image1.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image2.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image3.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image4.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image5.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image6.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image7.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image8.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image9.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image10.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image11.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image12.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image13.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image14.jpg', caption: "Default Caption" }
     ]
   }
 ];
