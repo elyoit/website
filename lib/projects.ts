a00a95b2b15335d2c955fd73644d0f5433cb1d91
elyoit <elliotnlai@gmail.com>
2026-06-10 16:22:45 -0700

    Update projects.ts

--- lib/projects.ts
+++ lib/projects.ts
@@ -2,7 +2,7 @@ export const projectsData = [
   {
     id: 'project1',
     title: 'Project 1',
-    heroImage: '/architecture/project1/image1.png',
+    heroImage: '/architecture/project1/hero.png',
     paragraphs: [""],
     gallery: [
       { src: '/architecture/project1/image1.png', caption: "Default Caption" },
@@ -15,12 +15,16 @@ export const projectsData = [
       { src: '/architecture/project1/image8.png', caption: "Default Caption" },
       { src: '/architecture/project1/image9.png', caption: "Default Caption" },
       { src: '/architecture/project1/image10.png', caption: "Default Caption" }
+      { src: '/architecture/project1/image11.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image12.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image13.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image14.png', caption: "Default Caption" },
     ]
   },
   {
     id: 'project2',
     title: 'Project 2',
-    heroImage: '/architecture/project2/image1.png',
+    heroImage: '/architecture/project2/hero.png',
     paragraphs: [""],
     gallery: [
       { src: '/architecture/project2/image1.png', caption: "Default Caption" },
@@ -31,26 +35,20 @@ export const projectsData = [
       { src: '/architecture/project2/image6.png', caption: "Default Caption" },
       { src: '/architecture/project2/image7.png', caption: "Default Caption" },
       { src: '/architecture/project2/image8.png', caption: "Default Caption" },
-      { src: '/architecture/project2/image9.png', caption: "Default Caption" },
-      { src: '/architecture/project2/image10.png', caption: "Default Caption" }
     ]
   },
   {
     id: 'project3',
     title: 'Project 3',
-    heroImage: '/architecture/project3/image1.png',
-    paragraphs: [""],
+    heroImage: '/architecture/project3/hero.jpg',
+    paragraphs: ["This project is a pavilion near Deep Eddy Pool in Austin."],
     gallery: [
-      { src: '/architecture/project3/image1.png', caption: "Default Caption" },
-      { src: '/architecture/project3/image2.png', caption: "Default Caption" },
-      { src: '/architecture/project3/image3.png', caption: "Default Caption" },
-      { src: '/architecture/project3/image4.png', caption: "Default Caption" },
-      { src: '/architecture/project3/image5.png', caption: "Default Caption" },
-      { src: '/architecture/project3/image6.png', caption: "Default Caption" },
-      { src: '/architecture/project3/image7.png', caption: "Default Caption" },
-      { src: '/architecture/project3/image8.png', caption: "Default Caption" },
-      { src: '/architecture/project3/image9.png', caption: "Default Caption" },
-      { src: '/architecture/project3/image10.png', caption: "Default Caption" }
+      { src: '/architecture/project3/image1.jpg', caption: "Default Caption" },
+      { src: '/architecture/project3/image2.jpg', caption: "Default Caption" },
+      { src: '/architecture/project3/image3.jpg', caption: "Default Caption" },
+      { src: '/architecture/project3/image4.jpg', caption: "Default Caption" },
+      { src: '/architecture/project3/image5.jpg', caption: "Default Caption" },
+      { src: '/architecture/project3/image6.jpg', caption: "Default Caption" },
     ]
   },
   {
