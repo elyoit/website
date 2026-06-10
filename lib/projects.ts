a63009ade15f99a9d15d6729c4d92ddacbe63682
elyoit <elliotnlai@gmail.com>
2026-06-10 15:58:34 -0700

    Update projects.ts

--- lib/projects.ts
+++ lib/projects.ts
@@ -1,162 +1,74 @@
 export const projectsData = [
-    {
+  {
     id: 'project1',
-    title: 'Project 1: Marble',
-    heroImage: '/architecture/Project1_Marble/LaiElliot_4.5_FinalModel_Detail5(4).JPG',
-    paragraphs: [
-            "BLANK DESCRIPTION NEW NEW!!!"
-        ],
+    title: 'Project 1',
+    heroImage: '/architecture/project1/image1.png',
+    paragraphs: [""],
     gallery: [
-            { 
-        src: '/architecture/Project1_Marble/LaiElliot_4.5_FinalModel_Detail5(4).JPG', 
-        caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project1_Marble/LaiElliot_4.5_FinalModel_Detail10(6).JPG', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project1_Marble/LaiElliot_4.5_FinalModel_Detail14(2).JPG', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project1_Marble/LaiElliot_4.5_FinalModel_Detail23(5).JPG', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project1_Marble/LaiElliot_4.5_FinalModel_Detail7(3).JPG', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project1_Marble/LaiElliot_4.5_FinalModel_View7(1).JPG', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project1_Marble/LaiElliot_4.5_FinalModel_View9(hero).JPG', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project1_Marble/LaiElliot_6.1_FinalModel_Paraline7.jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project1_Marble/LaiElliot_6.2_FinalModel_PlanSection8.jpg', caption: "BLANK CAPTION"
-            }
-        ]
-    },
-    {
+      { src: '/architecture/project1/image1.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image2.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image3.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image4.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image5.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image6.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image7.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image8.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image9.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image10.png', caption: "Default Caption" }
+    ]
+  },
+  {
     id: 'project2',
-    title: 'Project 2: Bathhouse',
-    heroImage: '/architecture/Project2_Bathhouse/LaiElliot_3.1_AnalogMap_Final(1).jpg',
-    paragraphs: [
-            "BLANK DESCRIPTION"
-        ],
+    title: 'Project 2',
+    heroImage: '/architecture/project2/image1.png',
+    paragraphs: [""],
     gallery: [
-            { src: '/architecture/Project2_Bathhouse/LaiElliot_3.1_AnalogMap_Final(1).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project2_Bathhouse/LaiElliot_3.2_DigitalMap_Final(2).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project2_Bathhouse/LaiElliot_3.4_Model2_View2(6).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project2_Bathhouse/LaiElliot_3.4_Model3_View(7).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project2_Bathhouse/LaiElliot_3.5_Bathhouse_Detail16(5).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project2_Bathhouse/LaiElliot_3.5_Bathhouse_Detail2(hero).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project2_Bathhouse/LaiElliot_3.5_Bathhouse_Detail6(4).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project2_Bathhouse/LaiElliot_3.5_Bathhouse_View(3).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project2_Bathhouse/LaiElliot_4D_Bathhouse_AxonFinal(10).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project2_Bathhouse/LaiElliot_4E_Bathhouse_PlanFinal(9).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project2_Bathhouse/LaiElliot_4E_Bathhouse_SectionFinal(8).jpg', caption: "BLANK CAPTION"
-            }
-        ]
-    },
-    {
+      { src: '/architecture/project2/image1.png', caption: "Default Caption" },
+      { src: '/architecture/project2/image2.png', caption: "Default Caption" },
+      { src: '/architecture/project2/image3.png', caption: "Default Caption" },
+      { src: '/architecture/project2/image4.png', caption: "Default Caption" },
+      { src: '/architecture/project2/image5.png', caption: "Default Caption" },
+      { src: '/architecture/project2/image6.png', caption: "Default Caption" },
+      { src: '/architecture/project2/image7.png', caption: "Default Caption" },
+      { src: '/architecture/project2/image8.png', caption: "Default Caption" },
+      { src: '/architecture/project2/image9.png', caption: "Default Caption" },
+      { src: '/architecture/project2/image10.png', caption: "Default Caption" }
+    ]
+  },
+  {
     id: 'project3',
-    title: 'Project 3: Pavilion',
-    heroImage: '/architecture/Project3_Pavilion/LaiElliot_2.1_Projection_Study2View3(1).jpg',
-    paragraphs: [
-            "BLANK DESCRIPTION"
-        ],
+    title: 'Project 3',
+    heroImage: '/architecture/project3/image1.png',
+    paragraphs: [""],
     gallery: [
-            { src: '/architecture/Project3_Pavilion/LaiElliot_2.1_Projection_Study2View3(1).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project3_Pavilion/LaiElliot_2.2_Projection_Axon(7).png', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project3_Pavilion/LaiElliot_2.2_Projection_ModelDetail13(5).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project3_Pavilion/LaiElliot_2.2_Projection_ModelDetail15(6).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project3_Pavilion/LaiElliot_2.2_Projection_ModelDetail25(3).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project3_Pavilion/LaiElliot_2.2_Projection_ModelDetail27(4).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project3_Pavilion/LaiElliot_2.2_Projection_ModelView5(2).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project3_Pavilion/LaiElliot_2.2_Projection_ModelView9(hero).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project3_Pavilion/LaiElliot_2.2_Projection_Plan1(9).png', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project3_Pavilion/LaiElliot_2.2_Projection_Plan2(8).png', caption: "BLANK CAPTION"
-            }
-        ]
-    },
-    {
+      { src: '/architecture/project3/image1.png', caption: "Default Caption" },
+      { src: '/architecture/project3/image2.png', caption: "Default Caption" },
+      { src: '/architecture/project3/image3.png', caption: "Default Caption" },
+      { src: '/architecture/project3/image4.png', caption: "Default Caption" },
+      { src: '/architecture/project3/image5.png', caption: "Default Caption" },
+      { src: '/architecture/project3/image6.png', caption: "Default Caption" },
+      { src: '/architecture/project3/image7.png', caption: "Default Caption" },
+      { src: '/architecture/project3/image8.png', caption: "Default Caption" },
+      { src: '/architecture/project3/image9.png', caption: "Default Caption" },
+      { src: '/architecture/project3/image10.png', caption: "Default Caption" }
+    ]
+  },
+  {
     id: 'project4',
-    title: 'Project 4: Design School',
-    heroImage: '/architecture/Project4_DesignSchool/LaiElliot_3_Integration_Axon(7).jpg',
-    paragraphs: [
-            "BLANK DESCRIPTION"
-        ],
+    title: 'Project 4',
+    heroImage: '/architecture/project4/image1.png',
+    paragraphs: [""],
     gallery: [
-            { src: '/architecture/Project4_DesignSchool/LaiElliot_3_Integration_Axon(7).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project4_DesignSchool/LaiElliot_3_Integration_ElevationSE(8).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project4_DesignSchool/LaiElliot_3_Integration_ModelDetail16(hero).JPG', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project4_DesignSchool/LaiElliot_3_Integration_ModelDetail21(12).JPG', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project4_DesignSchool/LaiElliot_3_Integration_ModelDetail22(5).JPG', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project4_DesignSchool/LaiElliot_3_Integration_ModelDetail28(4).JPG', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project4_DesignSchool/LaiElliot_3_Integration_ModelDetail35.JPG', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project4_DesignSchool/LaiElliot_3_Integration_ModelView1(2).JPG', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project4_DesignSchool/LaiElliot_3_Integration_ModelView13(3).JPG', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project4_DesignSchool/LaiElliot_3_Integration_RoofDiagram(11).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project4_DesignSchool/LaiElliot_3_Integration_SectionA(9).png', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project4_DesignSchool/LaiElliot_3_Integration_SectionE(10).png', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project4_DesignSchool/LaiElliot_3_Integration_SitePlan(6).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project4_DesignSchool/LaiElliot_3_Integration_StudyView3(1).JPG', caption: "BLANK CAPTION"
-            }
-        ]
-    },
-    {
-    id: 'project5',
-    title: 'Project 5: Multifamily',
-    heroImage: '/architecture/Project5_Multifamily/LaiElliot_4.0_Housing_Perspective20(6).jpg',
-    paragraphs: [
-            "BLANK DESCRIPTION"
-        ],
-    gallery: [
-            { src: '/architecture/Project5_Multifamily/LaiElliot_4.0_Housing_Perspective20(6).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project5_Multifamily/LaiElliot_4.0_Housing_Perspective23(5).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project5_Multifamily/LaiElliot_4.0_Housing_Perspective5(4).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project5_Multifamily/LaiElliot_4.0_Housing_Perspective9(3).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project5_Multifamily/LaiElliot_4.0_Housing_Render1(7).png', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project5_Multifamily/LaiElliot_4.0_Housing_Render2(8).png', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project5_Multifamily/LaiElliot_4.0_Housing_Render7(hero).png', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project5_Multifamily/LaiElliot_4.0_Housing_View1(1).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project5_Multifamily/LaiElliot_4.0_Housing_View5(2).jpg', caption: "BLANK CAPTION"
-            },
-            { src: '/architecture/Project5_Multifamily/LaiElliot_4.0_Housing_View9(2.5).jpg', caption: "BLANK CAPTION"
-            }
-        ]
-    }
+      { src: '/architecture/project4/image1.png', caption: "Default Caption" },
+      { src: '/architecture/project4/image2.png', caption: "Default Caption" },
+      { src: '/architecture/project4/image3.png', caption: "Default Caption" },
+      { src: '/architecture/project4/image4.png', caption: "Default Caption" },
+      { src: '/architecture/project4/image5.png', caption: "Default Caption" },
+      { src: '/architecture/project4/image6.png', caption: "Default Caption" },
+      { src: '/architecture/project4/image7.png', caption: "Default Caption" },
+      { src: '/architecture/project4/image8.png', caption: "Default Caption" },
+      { src: '/architecture/project4/image9.png', caption: "Default Caption" },
+      { src: '/architecture/project4/image10.png', caption: "Default Caption" }
+    ]
+  }
 ];
