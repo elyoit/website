961beb2ad091c17595982a2c073851b8feef4ea4
elyoit <elliotnlai@gmail.com>
2026-06-12 16:46:42 -0700

    Update photography.tsx

--- app/photography/photography.tsx
+++ app/photography/photography.tsx
@@ -1,3 +1,8 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+import { projectsData } from '@/lib/projects';
+
 export default function PhotographyPage() {
   return (
     <main className="p-10">
