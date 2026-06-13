66bc4403b6e379adc43271e0efbf2788fb1b3674
elyoit <elliotnlai@gmail.com>
2026-06-12 17:03:11 -0700

    poop

---
+++ lib/photography.ts
@@ -0,0 +1,76 @@
+export const projectsData = [
+  {
+    id: 'project1',
+    title: 'Living the Void',
+    heroImage: '/architecture/project1/hero.png',
+    paragraphs: [""],
+    gallery: [
+      { src: '/architecture/project1/image1.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image2.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image3.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image4.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image5.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image6.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image7.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image8.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image9.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image10.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image11.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image12.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image13.png', caption: "Default Caption" },
+      { src: '/architecture/project1/image14.png', caption: "Default Caption" }
+    ]
+  },
+  {
+    id: 'project2',
+    title: 'Curtain Wall Diagrams',
+    heroImage: '/architecture/project2/hero.png',
+    paragraphs: [""],
+    gallery: [
+      { src: '/architecture/project2/image1.png', caption: "Default Caption" },
+      { src: '/architecture/project2/image2.png', caption: "Default Caption" },
+      { src: '/architecture/project2/image3.png', caption: "Default Caption" },
+      { src: '/architecture/project2/image4.png', caption: "Default Caption" },
+      { src: '/architecture/project2/image5.png', caption: "Default Caption" },
+      { src: '/architecture/project2/image6.png', caption: "Default Caption" },
+      { src: '/architecture/project2/image7.png', caption: "Default Caption" },
+      { src: '/architecture/project2/image8.png', caption: "Default Caption" }
+    ]
+  },
+  {
+    id: 'project3',
+    title: 'Deep Eddy Pavilion',
+    heroImage: '/architecture/project3/hero.jpg',
+    paragraphs: ["This project is a pavilion near Deep Eddy Pool in Austin."],
+    gallery: [
+      { src: '/architecture/project3/image1.jpg', caption: "Default Caption" },
+      { src: '/architecture/project3/image2.jpg', caption: "Default Caption" },
+      { src: '/architecture/project3/image3.jpg', caption: "Default Caption" },
+      { src: '/architecture/project3/image4.jpg', caption: "Default Caption" },
+      { src: '/architecture/project3/image5.jpg', caption: "Default Caption" },
+      { src: '/architecture/project3/image6.jpg', caption: "Default Caption" }
+    ]
+  },
+  {
+    id: 'project4',
+    title: 'School of Tinkering',
+    heroImage: '/architecture/project4/hero.jpg',
+    paragraphs: ["This project is a design school near Deep Eddy Pool in Austin."],
+    gallery: [
+      { src: '/architecture/project4/image1.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image2.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image3.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image4.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image5.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image6.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image7.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image8.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image9.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image10.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image11.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image12.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image13.jpg', caption: "Default Caption" },
+      { src: '/architecture/project4/image14.jpg', caption: "Default Caption" }
+    ]
+  }
+];
