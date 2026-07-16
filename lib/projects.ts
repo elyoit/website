da5e291c2cd180e690fa90c77483e66564b5f76b
elyoit <elliotnlai@gmail.com>
2026-07-15 18:23:04 -0700

    Update projects.ts

--- lib/projects.ts
+++ lib/projects.ts
@@ -8,17 +8,17 @@ export const projectsData = [
       { src: '/architecture/project1/image1.png', caption: "" },
       { src: '/architecture/project1/image2.png', caption: "" },
       { src: '/architecture/project1/image3.png', caption: "" },
-      { src: '/architecture/project1/image4.png', caption: "" },
+      { src: '/architecture/project1/image4.png', caption: "Axonometric View" },
       { src: '/architecture/project1/image5.png', caption: "" },
       { src: '/architecture/project1/image6.png', caption: "" },
       { src: '/architecture/project1/image7.png', caption: "" },
       { src: '/architecture/project1/image8.png', caption: "" },
-      { src: '/architecture/project1/image9.png', caption: "" },
-      { src: '/architecture/project1/image10.png', caption: "" },
-      { src: '/architecture/project1/image11.png', caption: "" },
-      { src: '/architecture/project1/image12.png', caption: "" },
-      { src: '/architecture/project1/image13.png', caption: "" },
-      { src: '/architecture/project1/image14.png', caption: "" }
+      { src: '/architecture/project1/image9.png', caption: "Unfolded Section" },
+      { src: '/architecture/project1/image10.png', caption: "Site Plan" },
+      { src: '/architecture/project1/image11.png', caption: "Level 1" },
+      { src: '/architecture/project1/image12.png', caption: "Level 2" },
+      { src: '/architecture/project1/image13.png', caption: "Level 3" },
+      { src: '/architecture/project1/image14.png', caption: "Level 4" }
     ]
   },
   {
@@ -27,11 +27,11 @@ export const projectsData = [
     heroImage: '/architecture/project2/hero.png',
     paragraphs: ["Drafted and created in our Construction II lecture, these axonometric diagrams are case studies into the envelopes of buildings."],
     gallery: [
-      { src: '/architecture/project2/image1.png', caption: "" },
+      { src: '/architecture/project2/image1.png', caption: "Wood Wall Assembly" },
       { src: '/architecture/project2/image2.png', caption: "" },
       { src: '/architecture/project2/image3.png', caption: "" },
       { src: '/architecture/project2/image4.png', caption: "" },
-      { src: '/architecture/project2/image5.png', caption: "" },
+      { src: '/architecture/project2/image5.png', caption: "Concrete Wall Assembly" },
       { src: '/architecture/project2/image6.png', caption: "" },
       { src: '/architecture/project2/image7.png', caption: "" },
       { src: '/architecture/project2/image8.png', caption: "" }
@@ -43,11 +43,11 @@ export const projectsData = [
     heroImage: '/architecture/project3/hero.jpg',
     paragraphs: ["Before starting to design the third semester final project, a pavilion was designed next to the existing site in order to finalize the design intention. It features an overlapping roof allowing for variable shading conditions and pedestrian zones, dividing the space into a hierarchy of big communities, medium pockets, and intimate nooks."],
     gallery: [
-      { src: '/architecture/project3/image1.jpg', caption: "" },
+      { src: '/architecture/project3/image1.jpg', caption: "Composition Study" },
       { src: '/architecture/project3/image2.jpg', caption: "" },
-      { src: '/architecture/project3/image3.jpg', caption: "" },
-      { src: '/architecture/project3/image4.jpg', caption: "" },
-      { src: '/architecture/project3/image5.jpg', caption: "" },
+      { src: '/architecture/project3/image3.jpg', caption: "Axonometric" },
+      { src: '/architecture/project3/image4.jpg', caption: "Site Plan" },
+      { src: '/architecture/project3/image5.jpg', caption: "Floor Plan" },
       { src: '/architecture/project3/image6.jpg', caption: "" }
     ]
   },
@@ -57,20 +57,20 @@ export const projectsData = [
     heroImage: '/architecture/project4/hero.jpg',
     paragraphs: ["This project was defined by the programmatic word of field/array and the natural flow of people from the Deep Eddy Pool into Lake Austin. This axis of circulation in this building separates private from public, coffeeshop/foyer and workspace. These two divided spaces were not only physically interconnected with a basement level, but formally connected by the sawtooth roof, which borrows from the structural grid of this building."],
     gallery: [
-      { src: '/architecture/project4/image1.jpg', caption: "" },
+      { src: '/architecture/project4/image1.jpg', caption: "Curtain Wall Study Model" },
       { src: '/architecture/project4/image2.jpg', caption: "" },
       { src: '/architecture/project4/image3.jpg', caption: "" },
-      { src: '/architecture/project4/image4.jpg', caption: "" },
+      { src: '/architecture/project4/image4.jpg', caption: "Roof Detail" },
       { src: '/architecture/project4/image5.jpg', caption: "" },
-      { src: '/architecture/project4/image6.jpg', caption: "" },
-      { src: '/architecture/project4/image7.jpg', caption: "" },
-      { src: '/architecture/project4/image8.jpg', caption: "" },
-      { src: '/architecture/project4/image9.jpg', caption: "" },
-      { src: '/architecture/project4/image10.jpg', caption: "" },
-      { src: '/architecture/project4/image11.jpg', caption: "" },
-      { src: '/architecture/project4/image12.jpg', caption: "" },
-      { src: '/architecture/project4/image13.jpg', caption: "" },
-      { src: '/architecture/project4/image14.jpg', caption: "" }
+      { src: '/architecture/project4/image6.jpg', caption: "Axonometric" },
+      { src: '/architecture/project4/image7.jpg', caption: "Site Plan" },
+      { src: '/architecture/project4/image8.jpg', caption: "Southeast Elevation" },
+      { src: '/architecture/project4/image9.jpg', caption: "Section A" },
+      { src: '/architecture/project4/image10.jpg', caption: "Section E" },
+      { src: '/architecture/project4/image11.jpg', caption: "Roof Diagram" },
+      { src: '/architecture/project4/image12.jpg', caption: "Level 0" },
+      { src: '/architecture/project4/image13.jpg', caption: "Level 1" },
+      { src: '/architecture/project4/image14.jpg', caption: "Level 2" }
     ]
   }
 ];
